5b16d4da6c1565c92f3075ac4bca11a1f0b8541f
Noah Endtner <noah.endtner@web.de>
2024-08-24 17:40:20 +0200

    Added bitboards for attacking squares for each color, updated board to work with bitboards from a1 to h8

--- game/board/board.go
+++ game/board/board.go
@@ -1,25 +1,26 @@
 package board
 
 import (
+	"endtner.dev/nChess/game/boardhelper"
+	"endtner.dev/nChess/game/piece"
 	"fmt"
+	"math/bits"
+	"slices"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
 type Board struct {
-	whitePawns   uint64
-	whiteRooks   uint64
-	whiteKnights uint64
-	whiteBishops uint64
-	whiteQueens  uint64
-	whiteKing    uint64
+	// Bitboards are stored from bottom left to top right, meaning A1 to H8
+	// Bitboards are stored at the index of the piece they have
+	bitboards []uint64
 
-	blackPawns   uint64
-	blackRooks   uint64
-	blackKnights uint64
-	blackBishops uint64
-	blackQueens  uint64
-	blackKing    uint64
+	WhiteAttackFields uint64
+	BlackAttackFields uint64
+
+	whitePinnedPieces uint64
+	blackPinnedPieces uint64
 
 	whitePieces uint64
 	blackPieces uint64
@@ -27,7 +28,13 @@ type Board struct {
 }
 
 func New(fenString string) *Board {
+	// Since fen starts at a8, we want to split it at the /, reverse the list and join it back with /
+	fenRows := strings.Split(fenString, "/")
+	slices.Reverse(fenRows)
+	fenString = strings.Join(fenRows, "/")
+
 	b := Board{}
+	b.bitboards = make([]uint64, 0b11111)
 
 	// Setting up pieces
 	boardPosition := 0
@@ -53,128 +60,218 @@ func New(fenString string) *Board {
 		}
 
 		// Matching character, setting bits
-		switch rune(currentChar) {
-		case 'r':
-			b.blackRooks |= 1 << boardPosition
-		case 'n':
-			b.blackKnights |= 1 << boardPosition
-		case 'b':
-			b.blackBishops |= 1 << boardPosition
-		case 'q':
-			b.blackQueens |= 1 << boardPosition
-		case 'k':
-			b.blackKing |= 1 << boardPosition
-		case 'p':
-			b.blackPawns |= 1 << boardPosition
-
-		case 'R':
-			b.whiteRooks |= 1 << boardPosition
-		case 'N':
-			b.whiteKnights |= 1 << boardPosition
-		case 'B':
-			b.whiteBishops |= 1 << boardPosition
-		case 'Q':
-			b.whiteQueens |= 1 << boardPosition
-		case 'K':
-			b.whiteKing |= 1 << boardPosition
-		case 'P':
-			b.whitePawns |= 1 << boardPosition
-		default:
-			panic("Invalid char in fen string")
-		}
+		b.bitboards[piece.Value(rune(currentChar))] |= 1 << boardPosition
 
 		boardPosition++
 	}
 	// Setting the occupied fields
-	b.whitePieces = b.whiteRooks | b.whiteKnights | b.whiteBishops | b.whiteQueens | b.whiteKing | b.whitePawns
-	b.blackPieces = b.blackRooks | b.blackKnights | b.blackBishops | b.blackQueens | b.blackKing | b.blackPawns
+	for p, bitboard := range b.bitboards {
+		if uint(p)&piece.ColorWhite == piece.ColorWhite {
+			b.whitePieces |= bitboard
+		}
+		if uint(p)&piece.ColorBlack == piece.ColorBlack {
+			b.blackPieces |= bitboard
+		}
+	}
+
 	b.occupied = b.whitePieces | b.blackPieces
+
+	b.UpdateWhiteAttackBitboard()
+	b.UpdateBlackAttackBitboard()
 
 	return &b
 }
 
-func SquareToIndex(square string) uint {
-	if len(square) != 2 {
-		return 0
-	}
-
-	file := int(unicode.ToLower(rune(square[0]))) - int('a')
-	rank := 7 - (int(square[1]) - int('1'))
-
-	if file < 0 || file > 7 || rank < 0 || rank > 7 {
-		return 0
-	}
-
-	return uint(rank*8 + file)
+func (b *Board) PieceBitboard(piece uint) uint64 {
+	return b.bitboards[piece]
 }
 
-func IndexToSquare(index uint) string {
-	if index < 0 || index > 63 {
-		return ""
-	}
-
-	file := index % 8
-	rank := 7 - (index / 8)
-
-	return string(rune('a'+file)) + string(rune('1'+rank))
-}
-
-func (b *Board) WhitePawns() uint64 {
-	return b.whitePawns
-}
-
-func (b *Board) WhiteRooks() uint64 {
-	return b.whiteRooks
-}
-
-func (b *Board) WhiteKnights() uint64 {
-	return b.whiteKnights
-}
-
-func (b *Board) WhiteBishops() uint64 {
-	return b.whiteBishops
-}
-
-func (b *Board) WhiteQueens() uint64 {
-	return b.whiteQueens
-}
-
-func (b *Board) WhiteKing() uint64 {
-	return b.whiteKing
-}
-
-func (b *Board) BlackPawns() uint64 {
-	return b.blackPawns
-}
-
-func (b *Board) BlackRooks() uint64 {
-	return b.blackRooks
-}
-
-func (b *Board) BlackKnights() uint64 {
-	return b.blackKnights
-}
-
-func (b *Board) BlackBishops() uint64 {
-	return b.blackBishops
-}
-
-func (b *Board) BlackQueens() uint64 {
-	return b.blackQueens
-}
-
-func (b *Board) BlackKing() uint64 {
-	return b.blackKing
-}
-
-func (b *Board) OccupiedWhite() uint64 {
+func (b *Board) WhitePieces() uint64 {
 	return b.whitePieces
 }
 
-func (b *Board) OccupiedBlack() uint64 {
+func (b *Board) BlackPieces() uint64 {
 	return b.blackPieces
 }
 
 func (b *Board) Occupied() uint64 {
 	return b.occupied
+}
+
+func (b *Board) UpdateWhiteAttackBitboard() {
+	whitePawnAttacks := pawnAttackBitboard(b.PieceBitboard(piece.ColorWhite|piece.TypePawn), b.whitePieces, 8)
+	whiteStraightSlidingAttacks := straightSlidingAttackBitboard(
+		b.PieceBitboard(piece.ColorWhite|piece.TypeRook)|b.PieceBitboard(piece.ColorWhite|piece.TypeQueen),
+		b.whitePieces,
+		b.blackPieces,
+		8)
+	whiteStraightKingAttacks := straightSlidingAttackBitboard(
+		b.PieceBitboard(piece.ColorWhite|piece.TypeKing),
+		b.whitePieces,
+		b.blackPieces,
+		1)
+	whiteDiagonalSlidingAttacks := diagonalSlidingAttackBitboard(
+		b.PieceBitboard(piece.ColorWhite|piece.TypeBishop)|b.PieceBitboard(piece.ColorWhite|piece.TypeQueen),
+		b.whitePieces,
+		b.blackPieces,
+		8)
+	whiteDiagonalKingAttacks := diagonalSlidingAttackBitboard(
+		b.PieceBitboard(piece.ColorWhite|piece.TypeKing),
+		b.whitePieces,
+		b.blackPieces,
+		1)
+	whiteKnightAttacks := knightAttackBitboard(b.PieceBitboard(piece.ColorWhite|piece.TypeKnight), b.whitePieces)
+
+	b.WhiteAttackFields = whitePawnAttacks |
+		whiteStraightSlidingAttacks |
+		whiteStraightKingAttacks |
+		whiteDiagonalSlidingAttacks |
+		whiteDiagonalKingAttacks |
+		whiteKnightAttacks
+}
+
+func (b *Board) UpdateBlackAttackBitboard() {
+	blackPawnAttacks := pawnAttackBitboard(b.PieceBitboard(piece.ColorBlack|piece.TypePawn), b.blackPieces, -8)
+	blackStraightSlidingAttacks := straightSlidingAttackBitboard(
+		b.PieceBitboard(piece.ColorBlack|piece.TypeRook)|b.PieceBitboard(piece.ColorWhite|piece.TypeQueen),
+		b.blackPieces,
+		b.whitePieces,
+		8)
+	blackStraightKingAttacks := straightSlidingAttackBitboard(
+		b.PieceBitboard(piece.ColorBlack|piece.TypeKing),
+		b.blackPieces,
+		b.whitePieces,
+		1)
+	blackDiagonalSlidingAttacks := diagonalSlidingAttackBitboard(
+		b.PieceBitboard(piece.ColorBlack|piece.TypeBishop)|b.PieceBitboard(piece.ColorWhite|piece.TypeQueen),
+		b.blackPieces,
+		b.whitePieces,
+		8)
+	blackDiagonalKingAttacks := diagonalSlidingAttackBitboard(
+		b.PieceBitboard(piece.ColorBlack|piece.TypeKing),
+		b.blackPieces,
+		b.whitePieces,
+		1)
+	blackKnightAttacks := knightAttackBitboard(b.PieceBitboard(piece.ColorBlack|piece.TypeKnight), b.blackPieces)
+
+	b.BlackAttackFields = blackPawnAttacks |
+		blackStraightSlidingAttacks |
+		blackStraightKingAttacks |
+		blackDiagonalSlidingAttacks |
+		blackDiagonalKingAttacks |
+		blackKnightAttacks
+}
+
+func pawnAttackBitboard(pieceBitboard uint64, ownPiecesBitboard uint64, pawnIndexOffset int) uint64 {
+	var pawnAttacks uint64
+
+	for pieceBitboard != 0 {
+		startIndex := bits.TrailingZeros64(pieceBitboard)
+
+		side1 := startIndex + pawnIndexOffset - 1
+		side2 := startIndex + pawnIndexOffset + 1
+
+		if (side1 < 0 || side1 > 63) || (side2 < 0 || side2 > 63) {
+			continue
+		}
+
+		// Add move if it is in the same target row and targets are not empty
+		if side1/8 == (startIndex+pawnIndexOffset)/8 && !boardhelper.IsIndexBitSet(side1, ownPiecesBitboard) {
+			fmt.Printf("Pawn attack on %s\n", boardhelper.IndexToSquare(side1))
+			pawnAttacks |= 1 << side1
+		}
+		if side2/8 == (startIndex+pawnIndexOffset)/8 && !boardhelper.IsIndexBitSet(side2, ownPiecesBitboard) {
+			fmt.Printf("Pawn attack on %s\n", boardhelper.IndexToSquare(side2))
+			pawnAttacks |= 1 << side2
+		}
+
+		pieceBitboard &= pieceBitboard - 1
+	}
+
+	return pawnAttacks
+}
+
+func straightSlidingAttackBitboard(pieceBitboard uint64, ownPiecesBitboard uint64, enemyPiecesBitboard uint64, maxLength int) uint64 {
+	straightIndexOffsets := []int{1, -1, 8, -8}
+	var straightSlidingAttacks uint64
+
+	for pieceBitboard != 0 {
+		startIndex := bits.TrailingZeros64(pieceBitboard)
+
+		for _, offset := range straightIndexOffsets {
+			targetIndex := startIndex + offset
+
+			length := 0
+			// Go deep until we hit borders or our own pieces or maxLength
+			for length < maxLength && boardhelper.IsValidStraightMove(startIndex, targetIndex) && !boardhelper.IsIndexBitSet(targetIndex, ownPiecesBitboard) {
+
+				// We can go as deep as a capture
+				if boardhelper.IsIndexBitSet(targetIndex, enemyPiecesBitboard) {
+					straightSlidingAttacks |= 1 << targetIndex
+					break
+				}
+
+				straightSlidingAttacks |= 1 << targetIndex
+				targetIndex += offset
+				length++
+			}
+
+		}
+
+		pieceBitboard &= pieceBitboard - 1
+	}
+
+	return straightSlidingAttacks
+}
+
+func diagonalSlidingAttackBitboard(pieceBitboard uint64, ownPiecesBitboard uint64, enemyPiecesBitboard uint64, maxLength int) uint64 {
+	diagonalIndexOffsets := []int{-7, 7, -9, 9}
+	var diagonalSlidingAttacks uint64
+
+	for pieceBitboard != 0 {
+		startIndex := bits.TrailingZeros64(pieceBitboard)
+
+		for _, offset := range diagonalIndexOffsets {
+			targetIndex := startIndex + offset
+
+			length := 0
+			// Go deep until we hit borders or our own pieces or maxLength
+			for length < maxLength && boardhelper.IsValidDiagonalMove(startIndex, targetIndex) && !boardhelper.IsIndexBitSet(targetIndex, ownPiecesBitboard) {
+
+				// We can go as deep as a capture
+				if boardhelper.IsIndexBitSet(targetIndex, enemyPiecesBitboard) {
+					diagonalSlidingAttacks |= 1 << targetIndex
+					break
+				}
+
+				diagonalSlidingAttacks |= 1 << targetIndex
+				targetIndex += offset
+				length++
+			}
+
+		}
+
+		pieceBitboard &= pieceBitboard - 1
+	}
+
+	return diagonalSlidingAttacks
+}
+
+func knightAttackBitboard(pieceBitboard uint64, ownPiecesBitboard uint64) uint64 {
+	knightIndexOffsets := []int{-6, 6, -10, 10, -15, 15, -17, 17}
+	var knightAttacks uint64
+
+	for pieceBitboard != 0 {
+		startIndex := bits.TrailingZeros64(pieceBitboard)
+
+		for _, offset := range knightIndexOffsets {
+			if boardhelper.IsValidKnightMove(startIndex, startIndex+offset) && !boardhelper.IsIndexBitSet(startIndex+offset, ownPiecesBitboard) {
+				knightAttacks |= 1 << (startIndex + offset)
+			}
+		}
+
+		pieceBitboard &= pieceBitboard - 1
+	}
+
+	return knightAttacks
 }
